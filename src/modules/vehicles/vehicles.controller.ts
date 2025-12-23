3d0d8fca47cc59305caa1aa103a555d45b59adb6
anchal-1703 <anchalsandhu1703@gmail.com>
2025-12-23 15:31:03 +0530

    fix call center permissions

--- src/modules/vehicles/vehicles.controller.ts
+++ src/modules/vehicles/vehicles.controller.ts
@@ -21,31 +21,31 @@ export class VehiclesController {
     constructor(private readonly vehiclesService: VehiclesService) { }
 
     @Post()
-    @Roles('admin', 'sc_manager', 'service_advisor')
+    @Roles('admin', 'sc_manager', 'service_advisor', 'call_center')
     create(@Body() createVehicleDto: CreateVehicleDto) {
         return this.vehiclesService.create(createVehicleDto);
     }
 
     @Get()
-    @Roles('admin', 'sc_manager', 'service_advisor', 'service_engineer')
+    @Roles('admin', 'sc_manager', 'service_advisor', 'service_engineer', 'call_center')
     findAll(@Query() query: any) {
         return this.vehiclesService.findAll(query);
     }
 
     @Get(':id')
-    @Roles('admin', 'sc_manager', 'service_advisor', 'service_engineer')
+    @Roles('admin', 'sc_manager', 'service_advisor', 'service_engineer', 'call_center')
     findOne(@Param('id') id: string) {
         return this.vehiclesService.findOne(id);
     }
 
     @Get(':id/service-history')
-    @Roles('admin', 'sc_manager', 'service_advisor', 'service_engineer')
+    @Roles('admin', 'sc_manager', 'service_advisor', 'service_engineer', 'call_center')
     getServiceHistory(@Param('id') id: string, @Query() query: any) {
         return this.vehiclesService.getServiceHistory(id, query);
     }
 
     @Patch(':id')
-    @Roles('admin', 'sc_manager', 'service_advisor')
+    @Roles('admin', 'sc_manager', 'service_advisor', 'call_center')
     update(@Param('id') id: string, @Body() updateVehicleDto: UpdateVehicleDto) {
         return this.vehiclesService.update(id, updateVehicleDto);
     }
